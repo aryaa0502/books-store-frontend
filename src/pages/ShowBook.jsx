bf450e1aafdb9bbc71483d3a53090289c6e97eae
aryaa0502 <101689725+aryaa0502@users.noreply.github.com>
2023-09-08 17:07:45 +0530

    Update ShowBook.jsx

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://books-store-backend-l8ec.onrender.com/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
