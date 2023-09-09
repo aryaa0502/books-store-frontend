b7442479f9a8c195e2d0e5276c16fb463fcd23f8
aryaa0502 <101689725+aryaa0502@users.noreply.github.com>
2023-09-09 08:07:13 +0530

    Update BooksTable.jsx

--- src/components/home/BooksTable.jsx
+++ src/components/home/BooksTable.jsx
@@ -36,13 +36,13 @@ const BooksTable = ({ books }) => {
             </td>
             <td className='border border-slate-700 rounded-md text-center'>
               <div className='flex justify-center gap-x-4'>
-                <Link to={`/books/details/${book._id}`}>
+                <Link to={`/books/details/${book._id}`} title="Book Info">
                   <BsInfoCircle className='text-2xl text-green-800' />
                 </Link>
-                <Link to={`/books/edit/${book._id}`}>
+                <Link to={`/books/edit/${book._id}`} title="Edit Book">
                   <AiOutlineEdit className='text-2xl text-yellow-600' />
                 </Link>
-                <Link to={`/books/delete/${book._id}`}>
+                <Link to={`/books/delete/${book._id}`} title="Delete Book"> 
                   <MdOutlineDelete className='text-2xl text-red-600' />
                 </Link>
               </div>
